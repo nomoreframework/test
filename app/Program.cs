f90726d790dbeec4c5ff7f7d12bb19109e350ef6
nomoreframework <gleblukianenko@gmail.com>
2023-09-20 20:46:34 +0300

    init

---
+++ app/Program.cs
@@ -0,0 +1,26 @@
+﻿var print = (object msg) => Console.Write(msg + "\n");
+
+var getNumberOfLuckyTickets = (int dimension, int numberCount) => {
+	numberCount = (numberCount - numberCount % 2) / 2;
+	var sumsCount = new long [(dimension - 1)* numberCount + 1];
+	foreach(var iter in (long)Math.Pow(dimension,numberCount)){
+		long buff = iter;
+		long index = 0;
+		while(buff > 0){
+			index += buff % dimension;
+			buff /= dimension;
+		}
+		sumsCount[index]+=1;
+	}
+	long count = 0;
+	foreach(var i in sumsCount) count += i * i;
+return count;
+};
+print(getNumberOfLuckyTickets(13,13) * 13);
+static class longIncrementalExtention{
+	public static IEnumerator<long> GetEnumerator(this long n){
+		for(int i = 0; i < n;i++) yield return i;
+	}
+}
+
+
